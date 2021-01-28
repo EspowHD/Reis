6f9ac8c8d8e0a822d8c65da87f6b6531e2b1391c
EspowHD <espowhd@gmail.com>
2021-01-28 16:01:06 +0000

    Added getUser functionality

--- app/src/main/java/com/example/reis/repositories/DefaultMainRepository.kt
+++ app/src/main/java/com/example/reis/repositories/DefaultMainRepository.kt
@@ -2,6 +2,7 @@ package com.example.reis.repositories
 
 import android.net.Uri
 import com.example.reis.data.entities.Post
+import com.example.reis.data.entities.User
 import com.example.reis.other.Resource
 import com.example.reis.other.safeCall
 import com.google.firebase.auth.FirebaseAuth
@@ -31,15 +32,35 @@ class DefaultMainRepository : MainRepository {
             val imageUploadResult = storage.getReference(postId).putFile(imageUri).await()
             val imageUrl = imageUploadResult?.metadata?.reference?.downloadUrl?.await().toString()
             val post = Post(
-                    id = postId,
-                    authorUid = uid,
-                    text = text,
-                    imageUrl = imageUrl,
-                    date = System.currentTimeMillis(),
-                    taggedUsers = taggedUsers
+                id = postId,
+                authorUid = uid,
+                text = text,
+                imageUrl = imageUrl,
+                date = System.currentTimeMillis(),
+                taggedUsers = taggedUsers
             )
             posts.document(postId).set(post).await()
             Resource.Success(Any())
+        }
+    }
+
+    override suspend fun getUsers(uids: List<String>) = withContext(Dispatchers.IO) {
+        safeCall {
+            val usersList = users.whereIn("uid", uids).orderBy("username").get().await()
+                .toObjects(User::class.java)
+            Resource.Success(usersList)
+        }
+    }
+
+    override suspend fun getUser(uid: String) = withContext(Dispatchers.IO) {
+        safeCall {
+            val user = users.document(uid).get().await().toObject(User::class.java)
+                ?: throw IllegalStateException()
+            val currentUid = FirebaseAuth.getInstance().uid!!
+            val currentUser = users.document(currentUid).get().await().toObject(User::class.java)
+                ?: throw IllegalStateException()
+            user.isFollowing = uid in currentUser.follows
+            Resource.Success(user)
         }
     }
 }
--- app/src/main/java/com/example/reis/repositories/MainRepository.kt
+++ app/src/main/java/com/example/reis/repositories/MainRepository.kt
@@ -1,9 +1,14 @@
 package com.example.reis.repositories
 
 import android.net.Uri
+import com.example.reis.data.entities.User
 import com.example.reis.other.Resource
 
 interface MainRepository {
 
     suspend fun createPost(imageUri: Uri, text: String, taggedUsers: List<String>): Resource<Any>
+
+    suspend fun getUsers(uids: List<String>): Resource<List<User>>
+
+    suspend fun getUser(uid: String): Resource<User>
 }
