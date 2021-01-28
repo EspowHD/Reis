9377e455a580c7a84a046759dcdaa60380188f1f
EspowHD <espowhd@gmail.com>
2021-01-28 16:12:32 +0000

    Added getPostForFollows to get a list of all posts from the users that the current user follows

--- app/src/main/java/com/example/reis/repositories/MainRepository.kt
+++ app/src/main/java/com/example/reis/repositories/MainRepository.kt
@@ -1,6 +1,7 @@
 package com.example.reis.repositories
 
 import android.net.Uri
+import com.example.reis.data.entities.Post
 import com.example.reis.data.entities.User
 import com.example.reis.other.Resource
 
@@ -11,4 +12,6 @@ interface MainRepository {
     suspend fun getUsers(uids: List<String>): Resource<List<User>>
 
     suspend fun getUser(uid: String): Resource<User>
+
+    suspend fun getPostsForFollows(): Resource<List<Post>>
 }
