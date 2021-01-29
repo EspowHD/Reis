bcaabc0c5da87b9430ba0ae451c85b7f156f2e99
EspowHD <espowhd@gmail.com>
2021-01-29 08:41:06 +0000

    Added all code so posts from followed users should now be displayed can't currently test however as there is not yet functionality to follow other users

--- app/src/main/java/com/example/reis/ui/main/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/reis/ui/main/fragments/HomeFragment.kt
@@ -1,7 +1,58 @@
 package com.example.reis.ui.main.fragments
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.reis.R
+import com.example.reis.databinding.FragmentHomeBinding
+import com.example.reis.ui.main.viewmodels.BasePostViewModel
+import com.example.reis.ui.main.viewmodels.HomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+@AndroidEntryPoint
+class HomeFragment : BasePostFragment(R.layout.fragment_home) {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override val postProgressBar: ProgressBar
+        get() = binding.allPostsProgressBar
+
+    override val basePostViewModel: BasePostViewModel
+        get() {
+            val vm: HomeViewModel by viewModels()
+            return vm
+        }
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() = binding.rvAllPosts.apply {
+        adapter = postAdapter
+        layoutManager = LinearLayoutManager(requireContext())
+        itemAnimator = null
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
